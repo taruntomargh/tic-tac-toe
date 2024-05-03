d362a01c58e433ce04029792b4a87af3c9b5f5cb
Tarun Tomar <101380624+taruntomargh@users.noreply.github.com>
2024-05-03 19:08:05 +0530

    Added light/dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
+// import "./App.css";
 
 let arrValue = new Array(9);
 
 function App() {
-
   const [input, setInput] = useState("X");
   const [winner, setWinner] = useState("");
+  const [theme, setTheme] = useState("white");
 
   function handleGame(move) {
     setInput(input === "X" ? "O" : "X");
     arrValue[move] = input;
-    console.log(arrValue);
+    // console.log(arrValue);
 
     if (
       (arrValue[0] == "X" && arrValue[1] == "X" && arrValue[2] == "X") ||
@@ -38,61 +39,107 @@ function App() {
   }
 
   return (
-    <main style={{height: "100vh",display: "flex",alignItems: "center", justifyContent: "center", }}>
-      <div
+    <main
       style={{
-        width: "500px",
-        height: "500px",
+        height: "100vh",
         display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
         alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: `${theme}`
       }}
     >
-      <div style={{display: "flex"}}>
-        <button
-          style={{ width: "40px", height: "40px"}}
-          onClick={() => handleGame(0)}
-        >{arrValue[0]}</button>
-        <button
-          style={{ width: "40px", height: "40px" }}
-          onClick={() => handleGame(1)}
-        >{arrValue[1]}</button>
-        <button
-          style={{ width: "40px", height: "40px" }}
-          onClick={() => handleGame(2)}
-        >{arrValue[2]}</button>
+      <div
+        style={{
+          width: "500px",
+          height: "500px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center", gap: "50px"}}>
+          <h1 style={{color: `${theme === "white" ? "black" : "white"}`}}>TIC TAC TOE</h1>
+          <button
+            onClick={() => setTheme(theme === "white" ? "black" : "white")}
+            style={{ padding: "5px", color: "black", backgroundColor: "white", borderRadius: "4px"}}
+          >
+            {theme === "white" ? "dark" : "light"}
+          </button>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            
+          }}
+        >
+          <div style={{ display: "flex", }}>
+            <button
+              style={{ width: "40px", height: "40px", borderRight: "2px solid black", borderBottom: "2px solid black", borderLeft: "none", borderTop: "none", backgroundColor: "gray" }}
+              onClick={() => handleGame(0)}
+            >
+              {arrValue[0]}
+            </button>
+            <button
+              style={{ width: "40px", height: "40px" }}
+              onClick={() => handleGame(1)}
+            >
+              {arrValue[1]}
+            </button>
+            <button
+              style={{ width: "40px", height: "40px" }}
+              onClick={() => handleGame(2)}
+            >
+              {arrValue[2]}
+            </button>
+          </div>
+          <div style={{ display: "flex" }}>
+            <button
+              style={{ width: "40px", height: "40px" }}
+              onClick={() => handleGame(3)}
+            >
+              {arrValue[3]}
+            </button>
+            <button
+              style={{ width: "40px", height: "40px" }}
+              onClick={() => handleGame(4)}
+            >
+              {arrValue[4]}
+            </button>
+            <button
+              style={{ width: "40px", height: "40px" }}
+              onClick={() => handleGame(5)}
+            >
+              {arrValue[5]}
+            </button>
+          </div>
+          <div style={{ display: "flex" }}>
+            <button
+              style={{ width: "40px", height: "40px" }}
+              onClick={() => handleGame(6)}
+            >
+              {arrValue[6]}
+            </button>
+            <button
+              style={{ width: "40px", height: "40px" }}
+              onClick={() => handleGame(7)}
+            >
+              {arrValue[7]}
+            </button>
+            <button
+              style={{ width: "40px", height: "40px" }}
+              onClick={() => handleGame(8)}
+            >
+              {arrValue[8]}
+            </button>
+          </div>
+        </div>
+        <button style={{marginTop: "5px"}}>Play</button>
+        <span style={{color: `${theme === "white" ? "black" : "white"}`}}>{winner}</span>
       </div>
-      <div style={{display: "flex"}}>
-        <button
-          style={{ width: "40px", height: "40px" }}
-          onClick={() => handleGame(3)}
-        >{arrValue[3]}</button>
-        <button
-          style={{ width: "40px", height: "40px" }}
-          onClick={() => handleGame(4)}
-        >{arrValue[4]}</button>
-        <button
-          style={{ width: "40px", height: "40px" }}
-          onClick={() => handleGame(5)}
-        >{arrValue[5]}</button>
-      </div>
-      <div style={{display: "flex"}}>
-        <button
-          style={{ width: "40px", height: "40px" }}
-          onClick={() => handleGame(6)}
-        >{arrValue[6]}</button>
-        <button
-          style={{ width: "40px", height: "40px" }}
-          onClick={() => handleGame(7)}
-        >{arrValue[7]}</button>
-        <button
-          style={{ width: "40px", height: "40px" }}
-          onClick={() => handleGame(8)}
-        >{arrValue[8]}</button>
-      </div>
-      {winner}
-    </div>
     </main>
   );
 }
